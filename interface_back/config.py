3e5dccaf4f24a8869269315aa68e6481e1f1811a
Oleg Laptsay <74401728+Smail1k@users.noreply.github.com>
2024-01-24 16:04:28 +0400

    Create config.py

---
+++ interface_back/config.py
@@ -0,0 +1,8 @@
+SECRET_KEY = "exaple_secret_key"
+
+DB_HOST = "address.your.db.host"
+DRIVER = "ODBC Driver 17 for SQL Server"
+DB_USERNAME = "username"
+DB_PASSWORD = "password"
+DB_NAME = "db_name"
+DB_PORT = 1433
